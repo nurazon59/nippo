dc9fe8bcef8576e73e3e042ac7807e56e6804d50
nurazon59 <nurazon59@users.noreply.github.com>
2026-05-07 23:35:55 +0900

    feat: show cmd

---
+++ show.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type showCmd struct {
+	Date string `arg:"" required:"" help:"Target date (YYYY-MM-DD)."`
+}
+
+func (c *showCmd) Run() error {
+	cfg, err := loadConfig()
+	if err != nil {
+		return err
+	}
+
+	date, err := time.Parse("2006-01-02", c.Date)
+	if err != nil {
+		return err
+	}
+
+	storage, err := NewStorage(cfg.StorageDir)
+	if err != nil {
+		return err
+	}
+	content, err := storage.LoadReport(date)
+	if err != nil {
+		return err
+	}
+
+	fmt.Print(content)
+	return nil
+}
