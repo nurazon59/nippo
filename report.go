41b7755fb713d532a7339898ec9de80cb915c870
nurazon59 <nurazon59@users.noreply.github.com>
2026-05-07 22:34:31 +0900

    feat: report template

---
+++ report.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bytes"
+	"text/template"
+	"time"
+)
+
+type Report struct {
+	Date     time.Time
+	Done     []string
+	Todo     []string
+	Thoughts string
+}
+
+var reportTmpl = template.Must(template.New("report").Parse(`# 日報 {{.Date}}
+
+## やった
+{{range .Done}}- {{.Description}} ({{printf "%.1f" .Hours}}h)
+{{end}}
+## やる
+{{range .Todo}}- {{.}}
+{{end}}
+## 所感
+{{.Thoughts}}
+`))
+
+type templateData struct {
+	Date     string
+	Done     []string
+	Todo     []string
+	Thoughts string
+}
+
+func GenerateMarkdown(r *Report) string {
+	var buf bytes.Buffer
+	reportTmpl.Execute(&buf, templateData{
+		Date:     r.Date.Format("2006-01-02"),
+		Done:     r.Done,
+		Todo:     r.Todo,
+		Thoughts: r.Thoughts,
+	})
+	return buf.String()
+}
